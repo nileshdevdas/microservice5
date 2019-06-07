874599652925f1696e93f885cc454e9e269a3f8b
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2019-06-07 17:03:05 +0530

    DB

---
+++ src/main/java/com/example/demo/MyConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class MyConfig {
+
+}
--- src/main/java/com/example/demo/MyJob.java
+++ src/main/java/com/example/demo/MyJob.java
@@ -4,17 +4,32 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyJob {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Scheduled(fixedDelay = 5000)
 	public void processFiles() {
+		try {
+			Connection con = dataSource.getConnection();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 		File file = new File("d:/inputData/");
 		File[] files = file.listFiles();
 		if (files.length != 0) {
@@ -24,11 +39,11 @@ public class MyJob {
 				String line = "";
 				while ((line = reader.readLine()) != null) {
 					String[] cols = line.split(",");
-					if (cols[2].equals("\"large_airport\"") && cols[8].equals("\"IN\"")) {
+					if (cols[2].equals("\"small_airport\"") && cols[8].equals("\"IN\"")) {
 						smallAirports.add(line);
 					}
 				}
-				System.out.println("Total Large Airports = " + smallAirports.size());
+				System.out.println("Total Airports = " + smallAirports.size());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
